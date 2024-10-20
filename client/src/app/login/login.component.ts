b9e9315dd7387db68bab4f0293c8b50fb1560baa
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-10-19 22:58:36 -0400

    login and registration forms

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserSecurityService } from '../services/user-security.service';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    AsyncPipe
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
 
+  loginForm : FormGroup;
+  registrationForm : FormGroup;
+  message : BehaviorSubject<string>;
+
+
+  constructor(private formBuilder : FormBuilder, 
+    private userSecService : UserSecurityService, 
+    private router : Router){
+
+    this.message = new BehaviorSubject<string>('')
+
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.registrationForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  onLoginSubmit() {
+    let username = this.loginForm.get('username')?.value
+    let password = this.loginForm.get('password')?.value
+
+    if(this.userSecService.authenticateUser(username, password)) {
+      this.router.navigate(['home'])
+    } else {
+      this.message.next('Incorrect Credentials');
+    }
+    
+  }
+
+  onRegistrationSubmit() {
+    console.log(this.registrationForm.getRawValue());
+  }
 }
--- client/src/app/services/user-security.service.ts
+++ client/src/app/services/user-security.service.ts
@@ -13,6 +13,19 @@ export class UserSecurityService {
     this.currentUser = new BehaviorSubject<User|null>(null)
   }
 
+  authenticateUser(username : string, password : string) : boolean{
+    let usernameTest = "ronald"
+    let passwordTest = "123456789"
+    let userId = 1
+    let isValid = username === usernameTest && password === passwordTest
+    
+    if(isValid) {
+      this.currentUser.next(new User(userId, username));
+    }
+
+    return isValid   
+  }
+
   isAuthenticated() : Boolean {
     return this.currentUser.value != null
   }
